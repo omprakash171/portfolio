72795bc9902b398dda9ad521c1a178c2ca1dffcc
Om Prakash <bomprakash485@gmail.com>
2025-08-14 20:28:34 +0530

    responsive problem solved

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,37 +1,37 @@
-import { Component } from "react";
-
+import React, { Component } from "react";
 import "./index.css";
 
 class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { menuOpen: false };
+  }
+
+  toggleMenu = () => {
+    this.setState((prevState) => ({ menuOpen: !prevState.menuOpen }));
+  };
+
   render() {
     return (
       <nav className="navbar">
         <div className="logo">O.</div>
-        <ul className="nav-links">
-          <li>
-            <a href="#home">Home</a>
-          </li>
-          <li>
-            <a href="#about">About</a>
-          </li>
-          <li>
-            <a href="#projects">Projects</a>
-          </li>
-          <li>
-            <a href="#skills">Skills</a>
-          </li>
-          <li>
-            <a href="#learnings">Learning</a>
-          </li>
-          <li>
-            <a href="#blog">Blog</a>
-          </li>
-          <li>
-            <a href="#contact">Contact</a>
-          </li>
+
+        {/* Navigation Links */}
+        <ul className={`nav-links ${this.state.menuOpen ? "active" : ""}`}>
+          <li><a href="#home" onClick={this.toggleMenu}>Home</a></li>
+          <li><a href="#about" onClick={this.toggleMenu}>About</a></li>
+          <li><a href="#projects" onClick={this.toggleMenu}>Projects</a></li>
+          <li><a href="#skills" onClick={this.toggleMenu}>Skills</a></li>
+          <li><a href="#learnings" onClick={this.toggleMenu}>Learning</a></li>
+          <li><a href="#blog" onClick={this.toggleMenu}>Blog</a></li>
+          <li><a href="#contact" onClick={this.toggleMenu}>Contact</a></li>
           <button id="toggle-theme">🌓</button>
         </ul>
-        <div class="menu-toggle">&#9776;</div>
+
+        {/* Hamburger Icon */}
+        <div className="menu-toggle" onClick={this.toggleMenu}>
+          &#9776;
+        </div>
       </nav>
     );
   }
