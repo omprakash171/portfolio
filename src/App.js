5f75bbf9511d01eb3917a21d959135325d95a613
Om Prakash <bomprakash485@gmail.com>
2025-08-14 14:31:23 +0530

    added the about section

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Navbar from './components/Navbar';
 import Hero from './components/Hero'
+import About from './components/About';
 import './App.css';
 
 const App = () => {
@@ -7,6 +8,7 @@ const App = () => {
     <div className='app-container'>
       <Navbar />
       <Hero />
+      <About />
     </div>
   )
 }
