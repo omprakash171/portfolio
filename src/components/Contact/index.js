5253861dbc3c96dc3a1c2dc18b523288eec4949f
Om Prakash <bomprakash485@gmail.com>
2025-08-16 14:43:13 +0530

    added the working contact section

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,7 +1,35 @@
 import { Component } from "react";
 import "./index.css"; // Create this CSS file
+import Swal from "sweetalert2";
 
 class Contact extends Component {
+  onSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "fe5a06b9-d61f-4b8d-968e-4879db074e5c");
+
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+
+    const res = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: json,
+    }).then((res) => res.json());
+
+    if (res.success) {
+      Swal.fire({
+        title: "Success!",
+        text: "Message sent successfully!",
+        icon: "success",
+      });
+    }
+  };
+
   render() {
     return (
       <section className="contact-section" id="contact">
@@ -10,15 +38,25 @@ class Contact extends Component {
           Have a project in mind or just want to say hello? Fill out the form
           below and I’ll get back to you as soon as possible.
         </p>
-        <form className="contact-form">
+        <form className="contact-form" onSubmit={this.onSubmit}>
           <div className="form-group">
-            <input type="text" placeholder="Your Name" required />
+            <input type="text" placeholder="Your Name" name="name" required />
           </div>
           <div className="form-group">
-            <input type="email" placeholder="Your Email" required />
+            <input
+              type="email"
+              placeholder="Your Email"
+              name="email"
+              required
+            />
           </div>
           <div className="form-group">
-            <textarea placeholder="Your Message" rows="5" required></textarea>
+            <textarea
+              placeholder="Your Message"
+              rows="5"
+              name="message"
+              required
+            ></textarea>
           </div>
           <button type="submit" className="btn-submit">
             Send Message
